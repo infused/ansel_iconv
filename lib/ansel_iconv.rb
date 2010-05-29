df17561a674c4560d9691ee9c9619e383cd7079a
Keith Morrison <keithm@infused.org>
2010-05-28 18:11:39 -0700

    Unpack the char one time

--- lib/ansel_iconv.rb
+++ lib/ansel_iconv.rb
@@ -589,16 +589,17 @@ module ANSEL
       scanner = StringScanner.new(string)
       until scanner.eos? do
         byte = scanner.get_byte
+        char = byte.unpack('C')[0]
       
-        if byte.unpack('C')[0] <= 0x7F
+        if char <= 0x7F
           output << byte
-        elsif byte.unpack('C')[0] >= 0x88 && byte.unpack('C')[0] <= 0xC8
-          hex_key = byte.unpack('C')[0].to_s(16).upcase
+        elsif char >= 0x88 && char <= 0xC8
+          hex_key = char.to_s(16).upcase
           output << ::Iconv.conv(@to_charset, 'UTF-16', @ansi_to_utf8.has_key?(hex_key) ? @ansi_to_utf8[hex_key] : @ansi_to_utf8['ERR'])
           scanner.get_byte # ignore the next byte
-        elsif byte.unpack('C')[0] >= 0xE0 && byte.unpack('C')[0] <= 0xFB
+        elsif char >= 0xE0 && char <= 0xFB
           [2, 1, 0].each do |n| # try 3 bytes, then 2 bytes, then 1 byte
-            bytes = [byte.unpack('C')[0].to_s(16).upcase]
+            bytes = [char.to_s(16).upcase]
             scanner.peek(n).each_byte {|b| bytes << b.to_s(16).upcase}
             hex_key = bytes.join("+")
             if @ansi_to_utf8.has_key?(hex_key)  
