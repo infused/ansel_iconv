044b4a7f65a8a3fc08d81c747305f1afe4a4ec9e
Keith Morrison <keithm@infused.org>
2015-01-30 20:14:59 -0800

    rename ansi_to_utf8 method to ansi_to_utf16

--- lib/ansel_iconv/converter.rb
+++ lib/ansel_iconv/converter.rb
@@ -8,8 +8,8 @@ module ANSEL
       @to_charset = to_charset
     end
 
-    def ansi_to_utf8
-      @ansi_to_utf8 ||= @@non_combining.merge(@@combining)
+    def ansi_to_utf16
+      @ansi_to_utf16 ||= @@non_combining.merge(@@combining)
     end
 
     def convert(string)
@@ -24,21 +24,21 @@ module ANSEL
           output << byte.force_encoding('UTF-8')
         when 0x88..0xC8
           hex_key = char.to_s(16).upcase
-          output << (ansi_to_utf8[hex_key] || ansi_to_utf8['ERR']).force_encoding('UTF-16BE').encode('UTF-8')
+          output << (ansi_to_utf16[hex_key] || ansi_to_utf16['ERR']).force_encoding('UTF-16BE').encode('UTF-8')
           scanner.get_byte # ignore the next byte
         when 0xE0..0xFB
           [2, 1, 0].each do |n| # try 3 bytes, then 2 bytes, then 1 byte
             bytes = [char.to_s(16).upcase]
             scanner.peek(n).each_byte {|b| bytes << b.to_s(16).upcase}
             hex_key = bytes.join('+')
-            if ansi_to_utf8.has_key?(hex_key)
-              output << ansi_to_utf8[hex_key].force_encoding('UTF-16BE').encode('UTF-8')
+            if ansi_to_utf16.has_key?(hex_key)
+              output << ansi_to_utf16[hex_key].force_encoding('UTF-16BE').encode('UTF-8')
               n.times {scanner.get_byte}
               break
             end
           end
         else
-          output << ansi_to_utf8['ERR'].force_encoding('UTF-16BE').encode('UTF-8')
+          output << ansi_to_utf16['ERR'].force_encoding('UTF-16BE').encode('UTF-8')
           scanner.get_byte if scanner.get_byte.unpack('C')[0] >= 0xE0 # ignore the next byte
         end
       end
