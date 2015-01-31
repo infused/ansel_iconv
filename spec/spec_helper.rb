d4c4687dd9319a91c43cff5bc4ebc8e22b0b57ad
Keith Morrison <keithm@infused.org>
2015-01-30 20:20:39 -0800

    remove ruby-1.8 monkey patch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,14 +5,6 @@ require 'rubygems'
 require 'rspec'
 require 'ansel_iconv'
 
-# if RUBY_VERSION < '1.9'
-#   class String
-#     def force_encoding(e)
-#       self
-#     end
-#   end
-# end
-
 RSpec.configure do |config|
 
 end
